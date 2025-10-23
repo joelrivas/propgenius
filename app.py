13b3330e511889442b246c57bb9e590502fd71a5
Joel Rivas <joelarm@hotmail.com>
2025-10-22 19:08:13 -0600

    changed key

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from google import genai
 
-API_KEY = "AIzaSyCsM_TK4PgcJpmdDBSewaiU4BPgdYiLY_o"
+API_KEY = st.secrets["GEMINI_API_KEY"]
 client = genai.Client(api_key=API_KEY)
 
 st.title("PropGenius: Generador de Prompts de Venta Inmobiliaria")
